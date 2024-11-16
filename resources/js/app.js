389910f3a1a09b654afdb8d687f1b574a8c7793f
MarCap <cappellatomarco@yahoo.com>
2024-11-16 20:46:02 +0100

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+
+import "~resources/scss/app.scss";
+
+import "~resources/scss/partials/margins.scss";
+
+import "~resources/scss/partials/card.scss";
+
+// Direttiva per la gestione delle immagini
+import.meta.glob([
+    '../img/**'
+]);
